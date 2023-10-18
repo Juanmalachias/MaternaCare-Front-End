dc3712912acb945fbcf648b87e986f62d7a81317
lucas-nasc <lcsnascimento91@gmail.com>
2023-10-18 12:59:31 -0300

    crud 18/10

---
+++ src/components/produtos/formularioProdutos/FormularioProdutos.tsx
@@ -0,0 +1,190 @@
+import React, { ChangeEvent, useContext, useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { AuthContext } from '../../../contexts/AuthContext';
+import Produto from '../../../models/Produto';
+import Categoria from '../../../models/Categoria';
+import { buscar, atualizar, cadastrar } from '../../../service/Service';
+import { toastAlerta } from '../../../utils/toastAlerta';
+
+
+
+function FormularioProdutos() {
+  let navigate = useNavigate();
+
+  const { id } = useParams<{ id: string }>();
+
+  const { usuario, handleLogout } = useContext(AuthContext);
+  const token = usuario.token;
+
+  const [categorias, setCategorias] = useState<Categoria[]>([]);
+
+  const [categoria, setCategoria] = useState<Categoria>({
+    id: 0,
+    categoria: '',
+    descricao: '',
+  });
+
+  const [produto, setProduto] = useState<Produto>({
+    id: 0,
+    nomeProduto: '',
+    descricao: '',
+    preco: 0,
+    foto: '',
+    categoria: null,
+    usuario: null,
+  });
+
+  async function buscarProdutoPorId(id: string) {
+    await buscar(`/produtos/${id}`, setProduto, {
+      headers: {
+        Authorization: token,
+      },
+    });
+  }
+
+  async function buscarCategoriaPorId(id: string) {
+    await buscar(`/categoria/${id}`, setCategoria, {
+      headers: {
+        Authorization: token,
+      },
+    });
+  }
+
+  async function buscarCategoria() {
+    await buscar('/categoria', setCategoria, {
+      headers: {
+        Authorization: token,
+      },
+    });
+  }
+
+  useEffect(() => {
+    if (token === '') {
+      toastAlerta('Você precisa estar logado', 'info');
+      navigate('/');
+    }
+  }, [token]);
+
+  useEffect(() => {
+    buscarCategoria();
+    if (id !== undefined) {
+      buscarProdutoPorId(id);
+      console.log(categoria);
+
+    }
+  }, [id]);
+
+  useEffect(() => {
+    setProduto({
+      ...produto,
+      categoria: categoria,
+    });
+  }, [categoria]);
+
+  function atualizarEstado(e: ChangeEvent<HTMLInputElement>) {
+    setProduto({
+      ...produto,
+      [e.target.name]: e.target.value,
+      categoria: categoria,
+      usuario: usuario,
+    });
+  }
+
+  function retornar() {
+    navigate('/produtos');
+  }
+
+  async function gerarNovoProduto(e: ChangeEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    console.log({ produto });
+
+    if (id != undefined) {
+      try {
+        await atualizar(`/produtos`, produto, setProduto, {
+          headers: {
+            Authorization: token,
+          },
+        });
+        toastAlerta('Produto atualizado com sucesso', 'sucesso');
+        retornar();
+      } catch (error: any) {
+        if (error.toString().includes('403')) {
+          toastAlerta('O token expirou, favor logar novamente', 'info')
+          handleLogout()
+        } else {
+          toastAlerta('Erro ao atualizar a Postagem', 'erro');
+        }
+      }
+    } else {
+      try {
+        await cadastrar(`/produtos`, produto, setProduto, {
+          headers: {
+            Authorization: token,
+          },
+        });
+
+        toastAlerta('Produto cadastrado com sucesso', 'sucesso');
+        retornar();
+      } catch (error: any) {
+        if (error.toString().includes('403')) {
+          toastAlerta('O token expirou, favor logar novamente', 'info')
+          handleLogout()
+        } else {
+          toastAlerta('Erro ao cadastrar a Postagem', 'erro');
+        }
+      }
+    }
+  }
+
+  const carregandoCategoria = categoria.descricao === '';
+
+  return (
+    <div className="container flex flex-col mx-auto items-center">
+      <h1 className="text-4xl text-center my-8">{id !== undefined ? 'Editar Postagem' : 'Cadastrar Postagem'}</h1>
+
+      <form onSubmit={gerarNovoProduto} className="flex flex-col w-1/2 gap-4">
+        <div className="flex flex-col gap-2">
+          <label htmlFor="titulo">Titulo da postagem</label>
+          <input
+            value={produto.nomeProduto}
+            onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
+            type="text"
+            placeholder="Titulo"
+            name="titulo"
+            required
+            className="border-2 border-slate-700 rounded p-2"
+          />
+        </div>
+        <div className="flex flex-col gap-2">
+          <label htmlFor="titulo">Texto do produto</label>
+          <input
+            value={produto.descricao}
+            onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
+            type="text"
+            placeholder="Texto"
+            name="texto"
+            required
+            className="border-2 border-slate-700 rounded p-2"
+          />
+        </div>
+        <div className="flex flex-col gap-2">
+          <p>Categoria do produto</p>
+          <select name="categoria" id="categoria" className='border p-2 border-slate-800 rounded' onChange={(e) => buscarCategoriaPorId(e.currentTarget.value)}>
+            <option value="" selected disabled>Selecione um tema</option>
+            {categorias.map((categoria) => (
+              <>
+                <option value={categoria.id} >{categoria.descricao}</option>
+              </>
+            ))}
+          </select>
+        </div>
+        <button disabled={carregandoCategoria} type='submit' className='rounded disabled:bg-slate-200 bg-indigo-400 hover:bg-indigo-800 text-white font-bold w-1/2 mx-auto block py-2'>
+          {carregandoCategoria ? <span>Carregando</span> : id !== undefined ? 'Editar' : 'Cadastrar'}
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default FormularioProdutos;
